c403e94378258ccffcdf3dfe64904cdf34daa345
GeoWill <roper.william@gmail.com>
2025-04-01 16:50:51 +0100

    Use shellcheck

---
+++ scripts/code-check.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euxo pipefail
+
+uv run ruff format . --check
+uv run ruff check .
+
+if command -v shellcheck &> /dev/null; then
+  shellcheck scripts/*.sh
+else
+  echo "⚠️  shellcheck is not installed. Can't check shell scripts"
+fi
