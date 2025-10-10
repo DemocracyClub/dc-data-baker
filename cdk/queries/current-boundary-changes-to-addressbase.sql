5d511b94561259fdad41d193de7836ee5bcf1c98
symroe <symroe@users.noreply.github.com>
2025-10-10 07:05:54 +0100

    Initial spike of boundary reviews

---
+++ cdk/queries/current-boundary-changes-to-addressbase.sql
@@ -0,0 +1,32 @@
+UNLOAD (
+		SELECT results.uprn,
+			results.address,
+			results.postcode,
+			results.addressbase_source,
+			array_sort(filter(array_agg(DISTINCT results.review_id), x -> x IS NOT NULL)) AS boundary_review_ids,
+			results.first_letter AS first_letter
+		FROM (
+				SELECT ab.uprn,
+					ab.address,
+					ab.postcode,
+					ab.first_letter,
+					ab.addressbase_source,
+					cbc.review_id
+				FROM addressbase_partitioned ab
+					LEFT JOIN current_boundary_changes cbc ON ST_CONTAINS(
+						ST_Polygon(cbc.organisation_boundary_wkt),
+						ST_POINT(ab.longitude, ab.latitude)
+					)
+
+				WHERE ab.first_letter = '{first_letter}'
+			) AS results
+		GROUP BY results.uprn,
+			results.address,
+			results.postcode,
+			results.addressbase_source,
+			results.first_letter
+	) TO '$table_full_s3_path' WITH (
+		format = 'PARQUET',
+		compression = 'SNAPPY',
+		partitioned_by = ARRAY [ 'first_letter' ]
+	)
---
+++ cdk/shared_components/lambdas/create_boundary_changes_csv/create_current_boundary_reviews_csv.py
@@ -0,0 +1,77 @@
+import csv
+import io
+
+import boto3
+import psycopg
+
+ssm_client = boto3.client("ssm")
+
+host_response = ssm_client.get_parameter(
+    Name="/EveryElectionProd/DATABASE_HOST",
+)
+db_host = host_response["Parameter"]["Value"]
+
+password_response = ssm_client.get_parameter(
+    Name="/EveryElectionProd/DatabasePassword",
+)
+db_password = password_response["Parameter"]["Value"]
+
+
+def export_sql():
+    return """
+    SELECT
+        obr.id AS review_id,
+        obr.slug,
+        obr.status,
+        obr.latest_event,
+        obr.consultation_url,
+        obr.legislation_title,
+        obr.effective_date,
+        obr.created AS review_created,
+        obr.modified AS review_modified,
+        o.common_name AS organisation_name,
+        o.official_name AS organisation_official_name,
+        og_main.gss AS organisation_gss,
+        st_astext(ogs.geography) AS organisation_boundary_wkt
+    FROM organisations_organisationboundaryreview obr
+    LEFT JOIN organisations_organisation o ON o.id = obr.organisation_id
+    LEFT JOIN organisations_organisationgeography og_main ON og_main.organisation_id = o.id
+    LEFT JOIN organisations_organisationgeographysubdivided ogs ON ogs.organisation_geography_id = og_main.id
+    WHERE obr.status = 'CURRENT'
+    ORDER BY obr.created DESC, o.common_name;
+
+    """
+
+
+def handler(event, context):
+    db_name = "every_election"
+    db_user = "every_election"
+    db_port = "5432"
+
+    s3_bucket = "dc-data-baker-results-bucket"
+    s3_key = "current_boundary_reviews-with-wkt/current_boundary_reviews.csv"
+
+    query = export_sql()
+
+    conn = psycopg.connect(
+        host=db_host,
+        dbname=db_name,
+        user=db_user,
+        password=db_password,
+        port=db_port,
+    )
+    cur = conn.cursor()
+    cur.execute(query)
+    rows = cur.fetchall()
+
+    csv_buffer = io.StringIO()
+    csv_writer = csv.writer(csv_buffer)
+    csv_writer.writerows(rows)  # Write data rows
+
+    s3 = boto3.client("s3")
+    s3.put_object(Bucket=s3_bucket, Key=s3_key, Body=csv_buffer.getvalue())
+
+    cur.close()
+    conn.close()
+
+    return {"statusCode": 200, "body": "CSV successfully exported to S3."}
