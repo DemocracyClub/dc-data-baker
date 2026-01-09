f487a36e4925cf7db28675a60d713743c900fb82
GeoWill <roper.william@gmail.com>
2026-01-09 09:50:44 +0000

    s/boundary_review_ids/boundary_reviews
    The column is a list of boundary review objects, not ids.

--- cdk/queries/current-boundary-changes-to-addressbase.sql
+++ cdk/queries/current-boundary-changes-to-addressbase.sql
@@ -25,7 +25,7 @@ UNLOAD (
                array_agg(
                        '{{"boundary_review_id":' || json_format(CAST(boundary_review_id AS JSON)) ||
                        ',"changes":' || json_format(CAST(boundary_changes AS JSON)) || '}}'
-               ) AS boundary_review_ids
+               ) AS boundary_reviews
 		FROM grouped_by_review
 		GROUP BY uprn
 	)
@@ -34,7 +34,7 @@ UNLOAD (
 		aa.address,
 		aa.postcode,
 		aa.addressbase_source,
-           COALESCE(ar.boundary_review_ids, ARRAY[]) AS boundary_review_ids,
+           COALESCE(ar.boundary_reviews, ARRAY[]) AS boundary_reviews,
 		aa.first_letter
 	FROM all_addresses aa
 	LEFT JOIN aggregated_reviews ar ON aa.uprn = ar.uprn
