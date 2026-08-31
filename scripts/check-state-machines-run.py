dae211b56aef550624c8d30c75ba773bbaeea226
awdem <wdemontmollin@gmail.com>
2026-08-31 09:46:03 -0400

    add boundary changes coordinator stack to ci check

--- scripts/check-state-machines-run.py
+++ scripts/check-state-machines-run.py
@@ -22,6 +22,10 @@ class CheckStateMachinesRun:
             self.get_current_elections_state_machine_arn()
         )
 
+        self.current_boundary_changes_state_machine_arn = (
+            self.get_current_boundary_changes_state_machine_arn()
+        )
+
         self.sfn_client = boto3.client("stepfunctions")
 
     def handle(self):
@@ -29,6 +33,9 @@ class CheckStateMachinesRun:
         self.check_state_machine(self.addressbase_state_machine_arn)
         self.check_state_machine(
             self.current_elections_state_machine_arn, timeout=120
+        )
+        self.check_state_machine(
+            self.current_boundary_changes_state_machine_arn, timeout=240
         )
 
     def get_addressbase_state_machine_arn(self):
@@ -38,6 +45,14 @@ class CheckStateMachinesRun:
     def get_current_elections_state_machine_arn(self):
         current_elections_outputs = self.cdk_output["CurrentElectionsStack"]
         return current_elections_outputs["MakeCurrentElectionsParquetArnOutput"]
+
+    def get_current_boundary_changes_state_machine_arn(self):
+        current_boundary_changes_outputs = self.cdk_output[
+            "CurrentBoundaryChangesCoordinatorStack"
+        ]
+        return current_boundary_changes_outputs[
+            "MakeCurrentBoundaryChangesParquetArnOutput"
+        ]
 
     def check_state_machine(self, state_machine_arn, timeout=60):
         print(f"Checking {state_machine_arn}")
